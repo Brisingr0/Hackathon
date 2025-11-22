104bf213cc238e6bb01a1b19264520272da27f11
Brisingr0 <tryhard0131@gmail.com>
2025-11-22 17:52:12 -0500

    mood

--- Assets/Scripts/MoodController.cs
+++ Assets/Scripts/MoodController.cs
@@ -10,4 +10,84 @@ public class NewBehaviourScript : MonoBehaviour
         neutral,
         bad,
     }
+
+    [SerializeField] MoneyValuesSO moneyValuesSO;
+    private Mood mood;
+
+    public void SetMood(float money)
+    {
+        if (DialougeTracker.instance.IsNessie())
+        {
+            if (money/moneyValuesSO.NessieNecessitiesAmount > 1.05)
+            {
+                mood = Mood.good;
+            } else if (money/moneyValuesSO.NessieNecessitiesAmount < .95)
+            {
+                mood = Mood.bad;
+            } else
+            {
+                mood = Mood.neutral;
+            }
+        }
+        else if(DialougeTracker.instance.IsMisha())
+        {
+            if (money / moneyValuesSO.MishaLoanAmount > 1.05)
+            {
+                mood = Mood.good;
+            }
+            else if (money / moneyValuesSO.MishaLoanAmount < .95)
+            {
+                mood = Mood.bad;
+            }
+            else
+            {
+                mood = Mood.neutral;
+            }
+        }
+        else if (DialougeTracker.instance.IsRita())
+        {
+            if (money / moneyValuesSO.RitaRetirementAmount > 1.05)
+            {
+                mood = Mood.good;
+            }
+            else if (money / moneyValuesSO.MishaLoanAmount < .95)
+            {
+                mood = Mood.bad;
+            }
+            else
+            {
+                mood = Mood.neutral;
+            }
+        }
+        else if (DialougeTracker.instance.IsEllie())
+        {
+            if (money / moneyValuesSO.EllieEntertainmentAmount > 1.05)
+            {
+                mood = Mood.good;
+            }
+            else if (money / moneyValuesSO.EllieEntertainmentAmount < .95)
+            {
+                mood = Mood.bad;
+            }
+            else
+            {
+                mood = Mood.neutral;
+            }
+        }
+    }
+
+    public bool IsGoodMood()
+    {
+        return mood == Mood.good;
+    }
+
+    public bool IsNeutralMood()
+    {
+        return mood == Mood.neutral;
+    }
+
+    public bool IsBadMood()
+    {
+        return mood == Mood.bad;
+    }
 }
