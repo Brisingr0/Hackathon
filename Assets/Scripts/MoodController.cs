d59572854bbb666b9be02b88402021a8cb477964
Brisingr0 <tryhard0131@gmail.com>
2025-11-22 17:12:35 -0500

    small little chnages

---
+++ Assets/Scripts/MoodController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    enum Mood
+    {
+        good,
+        neutral,
+        bad,
+    }
+
+}
