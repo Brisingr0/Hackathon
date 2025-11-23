2a2e7c41e9659b5f80b0c0b9b65517babd72d276
Brisingr0 <tryhard0131@gmail.com>
2025-11-22 20:56:42 -0500

    backgrounds

---
+++ Assets/Scripts/BackgroundsSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BackgroundsSO : ScriptableObject
+{
+    public GameObject house;
+    public GameObject apartment;
+    public GameObject bridge;
+}
